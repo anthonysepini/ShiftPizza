97a98c20c262fbb3293ee03b0b6247fda8b95c63
Anthony Sepini <83968684+anthonysepini@users.noreply.github.com>
2026-08-13 23:38:29 -0300

    feat: register readiness health check

--- backend/src/modules/health/health.module.ts
+++ backend/src/modules/health/health.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { HealthService } from './application/health.service';
 import { HealthController } from './presentation/controllers/health.controller';
+import { ReadinessController } from './presentation/controllers/readiness.controller';
 
 @Module({
-  controllers: [HealthController],
+  controllers: [HealthController, ReadinessController],
+  providers: [HealthService],
 })
 export class HealthModule {}
